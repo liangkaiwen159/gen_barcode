d8b253d1e9a3b210e3cf8d2a8d4e3ac50343f41c
liangkaiwen159 <1373720113@qq.com>
2022-08-04 13:32:50 +0800

    optimize layout

--- pic2docx.py
+++ pic2docx.py
@@ -2,7 +2,8 @@ from docx import Document
 from docx.enum.table import WD_TABLE_ALIGNMENT, WD_CELL_VERTICAL_ALIGNMENT
 from docx.shared import Cm, Pt
 from docx.document import Document as Doc
-import os, sys
+import os
+import sys
 import time
 import datetime
 from docx2pdf import convert
@@ -17,7 +18,7 @@ class gen_docx():
             ':', '时')[5:16].replace(' ', '日').replace('-', '月') + '分-条形码输出'
         self.save_docx_path = os.path.join(os.getcwd(), current_time + '.docx')
         self.save_pdf_path = os.path.join(os.getcwd(), current_time + '.pdf')
-        self.document = Document()  #创建文档实例
+        self.document = Document()  # 创建文档实例
         sections = self.document.sections
         for section in sections:
             section.top_margin = Cm(1.27)
@@ -29,22 +30,28 @@ class gen_docx():
 
     def put_pic_in_word(self):
         for i, pic_path in enumerate(self.pic_paths):
-            if i + 1 <= 9:
-                cur_cell = self.table.cell(i, 0)
-                cur_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER  # 垂直居中
-                cur_cell.paragraphs[
-                    0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER  # 水平居中
-                run = cur_cell.paragraphs[0].add_run()  #添加图片的行
-                run.add_picture(pic_path, height=Cm(2.25))
-            else:
-                cur_cell = self.table.cell((i + 1) % 9 - 1, 1)
-                cur_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER  # 垂直居中
-                cur_cell.paragraphs[
-                    0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER  # 水平居中
-                run = cur_cell.paragraphs[0].add_run()  #添加图片的行
-                run.add_picture(pic_path, height=Cm(2.25))
+            # if i + 1 <= 9:
+            #     cur_cell = self.table.cell(i, 0)
+            #     cur_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER  # 垂直居中
+            #     cur_cell.paragraphs[
+            #         0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER  # 水平居中
+            #     run = cur_cell.paragraphs[0].add_run()  #添加图片的行
+            #     run.add_picture(pic_path, height=Cm(2.25))
+            # else:
+            #     cur_cell = self.table.cell((i + 1) % 9 - 1, 1)
+            #     cur_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER  # 垂直居中
+            #     cur_cell.paragraphs[
+            #         0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER  # 水平居中
+            #     run = cur_cell.paragraphs[0].add_run()  #添加图片的行
+            #     run.add_picture(pic_path, height=Cm(2.25))
+            cur_cell = self.table.cell(int(i/2), i % 2)
+            cur_cell.vertical_alignment = WD_CELL_VERTICAL_ALIGNMENT.CENTER  # 垂直居中
+            cur_cell.paragraphs[
+                0].paragraph_format.alignment = WD_TABLE_ALIGNMENT.CENTER  # 水平居中
+            run = cur_cell.paragraphs[0].add_run()  # 添加图片的行
+            run.add_picture(pic_path, height=Cm(2.25))
         self.document.save(self.save_docx_path)
-        #删除生成的条形码图片
+        # 删除生成的条形码图片
         for pic_path in self.pic_paths:
             os.remove(pic_path)
         convert(self.save_docx_path, self.save_pdf_path)
